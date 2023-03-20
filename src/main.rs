52bd077e5d649b6eeeb44b4f9ef51612185d6a74
ioni <104294646+IongIer@users.noreply.github.com>
2023-03-20 12:22:31 +0200

    Handle path to folder or file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod pgn_to_uhp;
 use args::CliArgs;
 use clap::Parser;
 use pgn_to_uhp::create_output_uhp_string;
+use std::fs;
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
@@ -11,7 +12,27 @@ fn main() -> std::io::Result<()> {
     let file_path = Path::new(&cli_args.path);
     let verbose = &cli_args.verbose;
     match &cli_args.mode {
-        args::Mode::Uhp => create_output_uhp_string(file_path, *verbose)?,
+        args::Mode::Uhp => {
+            if let Some(ext) = file_path.extension() {
+                if ext == "pgn" {
+                    create_output_uhp_string(file_path, *verbose)?
+                } else {
+                    panic!("File is not a PGN");
+                }
+            } else {
+                let files = fs::read_dir(file_path).unwrap();
+                files
+                    .filter_map(Result::ok)
+                    .filter(|d| {
+                        if let Some(ext) = d.path().extension() {
+                            ext == "pgn"
+                        } else {
+                            false
+                        }
+                    })
+                    .try_for_each(|f| create_output_uhp_string(f.path().as_ref(), *verbose))?;
+            }
+        }
         args::Mode::Pgn => {}
         args::Mode::Sgf => {}
     }
